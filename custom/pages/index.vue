0f54a28e6ee98e32489a915ce840d80ef609fa1d
Jonathan Martz <github@jmartz.de>
2025-06-25 23:19:40 +0200

    update index category

---
+++ custom/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="page">
+    <div class="mx-auto max-w-6xl grid grid-cols-6 gap-3">
+      <HomepageHero />
+      <CatalogProductGrid identifier="action-dvd" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import config from "./../pocketstore.json";
+const { t } = useI18n();
+useHead({
+    title: t("general.title"),
+});
+</script>
