5218c5324333cd15c25ea48163713dabdfce84b7
Jonathan Martz <github@jmartz.de>
2025-06-28 10:38:48 +0200

    update pwa and catalog with product

---
+++ custom/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="mx-auto max-w-6xl">
+        <section class="logo">
+            <a href="/">Logo</a>
+        </section>
+    <nav>
+Header with Logo and Nav
+    </nav>
+    </section>
+</template>
