210902dbd391bf0205f08e8c25165a0ff304e9bd
Jonathan Martz <github@jmartz.de>
2025-06-28 13:07:36 +0200

    update header

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,10 +1,25 @@
 <template>
-    <section class="mx-auto max-w-6xl">
+    <section class="mx-auto max-w-6xl flex justify-between">
         <section class="logo">
-            <a href="/">Logo</a>
+            <a href="/">
+                <img src="/pocketstore-favicon.svg" class="h-16" />
+            </a>
         </section>
     <nav>
-Header with Logo and Nav
+    <ul class="flex space-x-5 items-end mt-auto">
+        <li>
+            <a href="/">Startseite</a>
+        </li>
+        <li>
+            <a href="/de/checkout">Checkout</a>
+        </li>
+        <li>
+            <a href="/de/categories">Categories</a>
+        </li>
+        <li>
+            <a href="/de/brand/test-123">Brand</a>
+        </li>                
+    </ul>    
     </nav>
     </section>
 </template>
