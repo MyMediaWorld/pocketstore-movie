34d19b9334116bb364debd6c9ab2aa74199c638d
Jonathan Martz <github@jmartz.de>
2025-06-28 13:21:31 +0200

    header update

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -6,21 +6,21 @@
             </a>
         </section>
     <nav>
-    <ul class="flex space-x-5 items-end mt-auto">
+    <ul class="flex flex-col md:flex-row space-x-5">
         <li>
-            <a href="/">Startseite</a>
+            <a href="/" class="btn btn-sm btn-primary btn-block">Startseite</a>
         </li>
         <li>
-            <a href="/de/checkout">Checkout</a>
+            <a href="/de/checkout" class="btn btn-sm btn-primary btn-block">Checkout</a>
         </li>
         <li>
-            <a href="/de/categories">Categories</a>
+            <a href="/de/categories" class="btn btn-sm btn-primary btn-block">Categories</a>
         </li>
         <li>
-            <a href="/de/category/action-dvd.html">Categories</a>
+            <a href="/de/category/action-dvd.html" class="btn btn-sm btn-primary btn-block">Category</a>
         </li>
         <li>
-            <a href="/de/brand/test-123">Brand</a>
+            <a href="/de/brand/test-123" class="btn btn-sm btn-primary btn-block">Brand</a>
         </li>                
     </ul>    
     </nav>
