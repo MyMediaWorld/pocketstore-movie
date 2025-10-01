40265e9507f4635b7432325b781e664cb11b16b1
jonathan-martz <github@jmartz.de>
2025-10-01 21:00:41 +0200

    add disclaimer and template for adsense

--- custom/pages/index.vue
+++ custom/pages/index.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="page">
     <div class="mx-auto max-w-6xl grid grid-cols-6 gap-3">
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="1250525441896834"
+           data-ad-slot="YOUR_SLOT_ID"
+           data-ad-format="auto"></ins>
       <CatalogProductGrid identifier="action-dvd" />
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import {nextTick} from "../../storefront/.nuxt/imports";
+
 useHead({
   title: 'Media Store by JMSE - My-Media.world',
   meta: [
@@ -17,5 +24,18 @@ useHead({
         "My Media World, ist meine Welt der Medien gefüttert von TMDB mit Film Daten soll es erstmal zeigen was mit PocketStore.io möglich ist by JMSE.",
     },
   ],
+  script: [{
+    async: true,
+    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1250525441896834'
+  }]
+});
+
+onMounted(()=>{
+  nextTick(()=>{
+    console.log('tick: ',window.adsbygoogle);
+    if (window.adsbygoogle) {
+      window.adsbygoogle.push({});
+    }
+  })
 });
 </script>
