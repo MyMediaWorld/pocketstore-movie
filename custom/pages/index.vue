f77eb398a9116e198bb2cd07ca6d67b1b659ff2e
jonathan-martz <github@jmartz.de>
2025-12-01 19:08:07 +0100

    update index page

--- custom/pages/index.vue
+++ custom/pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <section class="page">
     <div class="mx-auto max-w-6xl grid grid-cols-6 gap-3">
-      <ins class="adsbygoogle"
-           style="display:block"
-           data-ad-client="1250525441896834"
-           data-ad-slot="YOUR_SLOT_ID"
-           data-ad-format="auto"></ins>
       <CatalogProductGrid identifier="action-dvd" />
     </div>
   </section>
@@ -22,18 +17,5 @@ useHead({
         "My Media World, ist meine Welt der Medien gefüttert von TMDB mit Film Daten soll es erstmal zeigen was mit PocketStore.io möglich ist by JMSE.",
     },
   ],
-  script: [{
-    async: true,
-    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1250525441896834'
-  }]
-});
-
-onMounted(()=>{
-  nextTick(()=>{
-    console.log('tick: ',window.adsbygoogle);
-    if (window.adsbygoogle) {
-      window.adsbygoogle.push({});
-    }
-  })
 });
 </script>
